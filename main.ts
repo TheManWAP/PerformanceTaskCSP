ba3e09eade099adb4bb7f8c953d6202cab915d1b
TheManWAP <145615525+TheManWAP@users.noreply.github.com>
2024-02-26 15:34:57 -0600

    Tutorial function has been replaced by an NPC function. Most likely replaced for more NPCs along the way.

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
 namespace SpriteKind {
     export const NPC = SpriteKind.create()
 }
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (!(Player1.isHittingTile(CollisionDirection.Top))) {
+        JumpCount = 0
+    }
+    if (Player1.isHittingTile(CollisionDirection.Left) || Player1.isHittingTile(CollisionDirection.Right)) {
+        Player1.vy = 20
+        JumpCount = 1
+    }
+})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     JumpCount += 1
     if (Player1.image.equals(assets.image`IdleDay1`)) {
@@ -27,15 +36,6 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     }
 })
-scene.onHitWall(SpriteKind.Player, function (sprite, location) {
-    if (!(Player1.isHittingTile(CollisionDirection.Top))) {
-        JumpCount = 0
-    }
-    if (Player1.isHittingTile(CollisionDirection.Left) || Player1.isHittingTile(CollisionDirection.Right)) {
-        Player1.vy = 20
-        JumpCount = 1
-    }
-})
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     TimeOfDay()
 })
@@ -52,19 +52,6 @@ function TimeOfDay () {
         Player1.setImage(assets.image`IdleDay1`)
         Player1.startEffect(effects.ashes, 1000)
         DayNightModifier = 1.5
-    }
-}
-function TutorialBasics () {
-    if (Player1.overlapsWith(Tutorial)) {
-        game.setDialogFrame(assets.image`SpeechBG`)
-        game.showLongText("It is a pleasure to meet your acquaintance. My name is Anna.", DialogLayout.Full)
-        game.showLongText("We do not have much time. The world id being torn apart by a dark energy.", DialogLayout.Full)
-        game.showLongText("I have granted you the ability to transform, but only during nighttime.", DialogLayout.Full)
-        game.showLongText("Using the (B) Button, you can fast-forward between day and night, each with its own perks/debuffs.", DialogLayout.Full)
-        game.showLongText("During the daytime, you are much faster, but lose an extra air jump and have weaker attacks.", DialogLayout.Full)
-        game.showLongText("During the nighttime, some of that speed is traded for an extra air jump and stronger attacks.", DialogLayout.Full)
-        game.showLongText("There is an obstacle course to help you learn your newfound powers.", DialogLayout.Full)
-        game.showLongText("The fate of the world rests in your hands. Good luck, hero.", DialogLayout.Full)
     }
 }
 controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
@@ -85,9 +72,25 @@ controller.left.onEvent(ControllerButtonEvent.Released, function () {
         speed = walkSpeed
     }
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    TutorialBasics()
+scene.onOverlapTile(SpriteKind.Player, assets.tile`PortalFG`, function (sprite, location) {
+    tiles.placeOnTile(Player1, tiles.getTileLocation(59, 55))
 })
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    TalkToNPC()
+})
+function TalkToNPC () {
+    if (Player1.overlapsWith(Tutorial)) {
+        game.setDialogFrame(assets.image`SpeechBG`)
+        game.showLongText("It is a pleasure to meet your acquaintance. My name is Anna.", DialogLayout.Full)
+        game.showLongText("We do not have much time. The world id being torn apart by a dark energy.", DialogLayout.Full)
+        game.showLongText("I have granted you the ability to transform, but only during nighttime.", DialogLayout.Full)
+        game.showLongText("Using the (B) Button, you can fast-forward between day and night, each with its own perks/debuffs.", DialogLayout.Full)
+        game.showLongText("During the daytime, you are much faster, but lose an extra air jump and have weaker attacks.", DialogLayout.Full)
+        game.showLongText("During the nighttime, some of that speed is traded for an extra air jump and stronger attacks.", DialogLayout.Full)
+        game.showLongText("There is an obstacle course to help you learn your newfound powers.", DialogLayout.Full)
+        game.showLongText("The fate of the world rests in your hands. Good luck, hero.", DialogLayout.Full)
+    }
+}
 function setRunningVariables () {
     speed = 80
     AccelerationValueDay = 10
@@ -110,6 +113,8 @@ let DayNightModifier = 0
 let JumpCount = 0
 let Player1: Sprite = null
 let Tutorial: Sprite = null
+game.splash("You seem to appear in a random plane. You are unsure of where you are, but there seems to be a mysterious lady with you.")
+game.splash("Press (DOWN) to talk to the mysterious lady behind you...")
 Tutorial = sprites.create(assets.image`TutorialLady`, SpriteKind.NPC)
 setRunningVariables()
 Player1 = sprites.create(assets.image`IdleDay1`, SpriteKind.Player)
@@ -122,6 +127,10 @@ JumpCount = 0
 DayNightModifier = 1.5
 tiles.placeOnTile(Tutorial, tiles.getTileLocation(5, 105))
 forever(function () {
+    if (Player1.tileKindAt(TileDirection.Center, sprites.builtin.brick) || Player1.tileKindAt(TileDirection.Center, sprites.dungeon.floorDark0)) {
+        game.splash("Try me bud.")
+        tiles.placeOnTile(Player1, tiles.getTileLocation(8, 103))
+    }
     if (Player1.image.equals(assets.image`IdleDay1`)) {
         if (speed >= 120) {
             speed = 120
